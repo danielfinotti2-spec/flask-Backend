26616649e281457876a115824d9f0737bd4de97f
Daniel Finotti <danielfinotti2@gmail.com>
2026-09-18 20:26:44 -0300

    atualizaçao

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template
 
+# Criação do objeto Flask e iniciando a aplicação
 app = Flask(__name__)
 
+# Flask Rota Pagina Inicial
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route("/Wiki.html")
+def Wiki():
+    return render_template("Wiki.html")
 
+@app.route("/Contatos.html")
+def contatos():
+    return render_template("Contatos.html")
+
+@app.route("/index.html")
+def index():
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
